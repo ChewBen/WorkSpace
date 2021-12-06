4a6458834e9ca920f1b4086ba6db9c3c5a5aea92
chewy <haianben@outlook.com>
2021-12-06 10:44:03 +0800

    Merge remote-tracking branch 'origin/main'

--- weather/index.js
+++ weather/index.js
@@ -1,5 +1,19 @@
-function sum(a, b) {
-    return a + b
+const axios = require("axios")
+
+function getWeather(city) {
+    return new Promise((resolve, reject) => {
+        axios.get('https://api2.jirengu.com/getWeather.php?city=${city} ')
+            .then(response => {
+                console.log(response)
+                resolve(response)
+            })
+            .catch(e=> {
+                reject=> ('网络异常')
+            })
+    })
+        .then(function () {
+            // always executed
+        })
 }
 
-module.exports = sum
+module.exports = getWeather
